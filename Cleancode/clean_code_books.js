ade652c08ad5cfb43977a7dbe1728efac4a564b6
Rytryx <imrytry@gmail.com>
2023-09-15 13:35:26 +0200

    Fixing the Code after Clean-Code

--- Cleancode/clean_code_books.js
+++ Cleancode/clean_code_books.js
@@ -1,42 +1,34 @@
-// Poorly written JavaScript file for handling book transactions
+let books = [
+  { id: 1, booktitle: "Der alte Mann und das Meer", price: 10.5 },
+  { id: 2, booktitle: "Moby Dick", price: 12.3 },
+  { id: 3, booktitle: "1984", price: 8.5 },
+];
 
-let bks = [
-    { i: 1, n: "Der alte Mann und das Meer", p: 10.5 },
-    { i: 2, n: "Moby Dick", p: 12.3 },
-    { i: 3, n: "1984", p: 8.5 },
-  ];
-  
-  function getB(id) {
-    for (let b of bks) {
-      // Compares two values
-      if (b.i == id) {
-        return b;
-      }
-    }
-    // Returns null
-    return null;
-  }
-  
-  function aB(name, price) {
-    // The maxId
-    let maxId = 0;
-    for (let b of bks) {
-      if (b.i > maxId) maxId = b.i;
-    }
-    // Pushes a new item into an array
-    bks.push({ i: maxId + 1, n: name, p: price });
-  }
-  
-  function lB() {
-    for (let b of bks) {
-      // Outputs something to the console
-      console.log(b.n + " - " + b.p + "€");
+function getBookById(id) {
+  for (let book of books) {
+    if (book.id === id) {
+      return book;
     }
   }
-  
-  // Example Function Calls
-  let myB = getB(2);
-  console.log(myB);
-  aB("Fahrenheit 451", 9.2);
-  lB();
-  
+  return null; 
+}
+function addNewBook(title, price) {
+  let maxId = 0;
+  for (let book of books) {
+    if (book.id > maxId) {
+      maxId = book.id;
+    }
+  }
+  books.push({ id: maxId + 1, booktitle: title, price: price });
+}
+
+function listAllBooks() {
+  for (let book of books) {
+    console.log(book.booktitle + " - " + book.price + "€");
+  }
+}
+
+let myBook = getBookById(2);
+console.log(myBook);
+addNewBook("Fahrenheit 451", 9.2);
+listAllBooks();
